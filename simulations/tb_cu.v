aaeb42d467a72547f45a0b32e6505e176666433e
Mohamed Abdelmagid <68608858+MohamedEbrahem1@users.noreply.github.com>
2023-11-28 15:30:35 +0200

    Updated Control Unit

--- simulations/tb_cu.v
+++ simulations/tb_cu.v
@@ -2,53 +2,40 @@
 `include "../src/cu.v"
 module ControlUnit_tb;
 
-    reg [6:0] opcode;
-    reg [2:0] funct3;
-    reg [6:0] funct7;
+  reg [6:2] opcode;
 
-    wire RegWrite, MemRead, MemWrite, ALUSrc;
-    wire [2:0] ALUOp;
-    wire Branch, Jump, MemToReg;
-    wire [1:0] RegDst;
+  wire Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
+  wire [1:0] ALUOp;
 
-    // Instantiate the ControlUnit module
-    ControlUnit uut (
-        .opcode(opcode),
-        .funct3(funct3),
-        .funct7(funct7),
-        .RegWrite(RegWrite),
-        .MemRead(MemRead),
-        .MemWrite(MemWrite),
-        .ALUSrc(ALUSrc),
-        .ALUOp(ALUOp),
-        .Branch(Branch),
-        .Jump(Jump),
-        .MemToReg(MemToReg),
-        .RegDst(RegDst)
-    );
+  ControlUnit uut (
+    .opcode(opcode),
+    .Branch(Branch),
+    .MemRead(MemRead),
+    .MemtoReg(MemtoReg),
+    .MemWrite(MemWrite),
+    .ALUSrc(ALUSrc),
+    .RegWrite(RegWrite),
+    .ALUOp(ALUOp)
+  );
 
-    // Initial values
-    initial begin
-        $dumpfile("control_unit_tb.vcd");
-        $dumpvars(0, ControlUnit_tb);
+  initial begin
+    // Test case 1
+    opcode = 5'b01100;
+    #10; 
 
-        // Test case 1: R-Type ADD instruction
-        opcode = 7'b0110011;
-        funct3 = 3'b000;
-        funct7 = 7'b0000000;
-        #10 $display("Control Signals for R-Type ADD: RegWrite=%b, MemRead=%b, MemWrite=%b, ALUSrc=%b, ALUOp=%b, Branch=%b, Jump=%b, MemToReg=%b, RegDst=%b",
-                    RegWrite, MemRead, MemWrite, ALUSrc, ALUOp, Branch, Jump, MemToReg, RegDst);
+    // Test case 2
+    opcode = 5'b00000;
+    #10;
 
-        // Test case 2: I-Type ORI instruction
-        opcode = 7'b0010011;
-        funct3 = 3'b111;
-        funct7 = 7'b0000000;
-        #10 $display("Control Signals for I-Type ORI: RegWrite=%b, MemRead=%b, MemWrite=%b, ALUSrc=%b, ALUOp=%b, Branch=%b, Jump=%b, MemToReg=%b, RegDst=%b",
-                    RegWrite, MemRead, MemWrite, ALUSrc, ALUOp, Branch, Jump, MemToReg, RegDst);
+    // Test case 3
+    opcode = 5'b01000;
+    #10;
 
-        // Add more test cases as needed
+    // Test case 4
+    opcode = 5'b11000;
+    #10;
 
-        #10 $finish;
-    end
+    $finish;
+  end
 
 endmodule
